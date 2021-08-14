f9e6a9472f5c97b4824a047c96a042190ac943ec
JiajunLi <jiajun.li@hometrack.com.au>
2021-08-14 17:45:10 +0800

    lint

--- puppeteer.js
+++ puppeteer.js
@@ -1,55 +1,55 @@
-const puppeteer = require ('puppeteer');
+const puppeteer = require('puppeteer');
 const sound = require('sound-play')
 const open = require('open');
 const schedule = require('node-schedule')
 let JDUrl = 'https://item.jd.com/100022493728.html';
 let microsoftPageUrl = 'https://surface.wiki/';
 
-const getFromMicrosoft = async() => {
+const getFromMicrosoft = async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto (microsoftPageUrl);
-  await page.waitForSelector ('body');
-  let grabPosts = await page.evaluate (() => {
-    let xboxInformation = document.body.querySelectorAll ('[href="/go/xbox-series-x-configurate"]');
+  const MSPage = await browser.newPage();
+  await MSPage.goto(microsoftPageUrl);
+  await MSPage.waitForSelector('body');
+  let grabPosts = await MSPage.evaluate(() => {
+    let xboxInformation = document.body.querySelectorAll('[href="/go/xbox-series-x-configurate"]');
     let scrapeItems = [];
-    xboxInformation.forEach (information => {
-      let text = information.querySelectorAll ('div');
+    xboxInformation.forEach(information => {
+      let text = information.querySelectorAll('div');
       text.forEach((title) => {
         scrapeItems.push(title.innerText)
       })
     });
     return scrapeItems;
   });
-  await browser.close ();
+  await browser.close();
   return grabPosts;
 }
 
 const getFromJd = async () => {
   const browser = await puppeteer.launch();
-  const jdPage = await browser.newPage ();
-  await jdPage.goto (JDUrl);
-  await jdPage.waitForSelector ('body');
+  const jdPage = await browser.newPage();
+  await jdPage.goto(JDUrl);
+  await jdPage.waitForSelector('body');
 
-  let JDPosts = await jdPage.evaluate (() => {
-    let xboxInformation = document.body.querySelectorAll ('[class="itemover-tip"]');
+  let JDPosts = await jdPage.evaluate(() => {
+    let xboxInformation = document.body.querySelectorAll('[class="itemover-tip"]');
 
     let jdCart = [];
-    xboxInformation.forEach (information => {
+    xboxInformation.forEach(information => {
       let text = information.innerText;
       jdCart.push(text)
     });
     return jdCart;
   });
-  await browser.close ();
+  await browser.close();
   return JDPosts;
 }
 
 let MSResult = [];
 let JDResult = [];
-const job =schedule.scheduleJob('* * * * *', async function(){
-    MSResult = await getFromMicrosoft();
-    JDResult = await getFromJd();
+const job = schedule.scheduleJob('* * * * *', async function () {
+  MSResult = await getFromMicrosoft();
+  JDResult = await getFromJd();
   console.log('MSResult looks like', MSResult);
   console.log('JDResult looks like', JDResult);
   if (MSResult.includes("去购买")) {
@@ -57,7 +57,7 @@ const job =schedule.scheduleJob('* * * * *', async function(){
     console.log("success!!!!!")
     sound.play('peaches.mp3');
     job.cancel()
-  } else if (!JDResult.includes('该商品已下柜，欢迎挑选其他商品！')){
+  } else if (!JDResult.includes('该商品已下柜，欢迎挑选其他商品！')) {
     open(JDUrl);
     console.log("success!!!!!")
     sound.play('peaches.mp3');
